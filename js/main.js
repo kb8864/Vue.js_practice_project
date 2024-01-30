b3cbac4b9b3b19b6b21b6013125267206cc1d009
kb8864 <yt7s858864@gmail.com>
2024-01-30 22:46:18 +0900

    v-oneディレクティブ

--- js/main.js
+++ js/main.js
@@ -1,29 +1,10 @@
 const app = Vue.createApp({
   data: () => ({
-    newItem: '',
-    // イベントハンドラであるaddItemメソッドでinput要素に入力されている値とデータバインディングされているnewItemの値を配列に追加する処理を記述
-
-    todos: [],
+    message: 'Hello, Vue.js',
   }),
   methods: {
-    addItem: function (event) {
-      // console.log('Clicked!');
-      // 下記の記述がないとタスクが未入力でも配列に追加されてしまう
-      if (this.newItem === '') return;
-      let todo = {
-        item: this.newItem,
-        isDone: false, // task完了、未完了の真偽地
-      };
-      this.todos.push(todo);
-      this.newItem = '';
-    },
-    deleteItem: function (index) {
-      //引数がindexなのは、deleteItemメソッドから見たときにどの削除ボタンが押されたか判別する必要があり、そこで deleteItemメソッドの引数に配列のindexを渡す
-      console.log('delete');
-      console.log('index');
-
-      //作成したタスクは配列に追加されるので、配列からから要素を削除する.spliceを使用
-      this.todos.splice(index, 1);
+    clickHandler: function (event) {
+      this.message = this.message.split('').reverse().join('');
     },
   },
 });
