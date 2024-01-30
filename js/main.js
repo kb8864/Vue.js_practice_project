8138a0b41431649e48a45e4a6451e62d8dcc3fbe
kb8864 <yt7s858864@gmail.com>
2024-01-30 17:58:55 +0900

    add:タスク追加処理を追加

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 const app = Vue.createApp({
-  data: () => ({}),
+  data: () => ({
+    newItem: '',
+    // イベントハンドラであるaddItemメソッドでinput要素に入力されている値とデータバインディングされているnewItemの値を配列に追加する処理を記述
+
+    todos: [],
+  }),
   methods: {
     addItem: function (event) {
       console.log('Clicked!');
+      let todo = {
+        item: this.newItem,
+      };
+      this.todos.push(todo);
     },
   },
 });
