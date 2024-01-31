c8ed11dd30429d1b67b66929a7806543fdf0e0e9
kb8864 <yt7s858864@gmail.com>
2024-01-31 15:34:19 +0900

    算出プロパティの学習

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
 const app = Vue.createApp({
   data: () => ({
-    message: 'Hello, Vue.js',
+    // message: 'Hello, Vue.js',
   }),
+  //computedは、キャッシュがある。messageプロパティが変わらない限りは
+  // reversedMessageに何度アクセスしても関数を再び実行することなく
+  // 以前計算された結果を瞬時に返します
+  // computed: {
+  //   reversedMessage: function () {
+  //     return this.message.split('').reverse().join('');
+  //   },
+  // },
+
+  computed: {
+    computedNumber: function () {
+      console.log('computed!');
+      return Math.random();
+    },
+  },
+
+  // methodsはキャッシュなし。呼び出されるたびに関数の処理を行って処理を返します
+  // methods: {
+  //   reversedMessageMethod: function () {
+  //     return this.message.split('').reverse().join('');
+  //   },
+  // },
+
+  methods: {
+    methodsNumber: function () {
+      console.log('methods!');
+      return Math.random();
+    },
+  },
 });
 app.mount('#app');
