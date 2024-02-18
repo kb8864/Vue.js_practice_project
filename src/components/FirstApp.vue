77e31d347e4f51116142a9156de3a0cfe0e0ef79
kb8864 <yt7s858864@gmail.com>
2024-02-18 13:23:16 +0900

    Hello world

---
+++ src/components/FirstApp.vue
@@ -0,0 +1,13 @@
+<script setup>
+// テンプレート変数を定義するには、ref関数に初期値を渡すだけ
+import { ref } from 'vue'
+//ビュー変数を準備
+const message=ref('Hello world')
+</script>
+
+<template>
+  <p>{{ message }}</p>
+</template>
+
+<style scoped>
+</style>
