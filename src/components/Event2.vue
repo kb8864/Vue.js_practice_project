54d7d1d8dfa4155bd1f96fcf171d07ff6621a8bc
kb8864 <yt7s858864@gmail.com>
2024-02-20 18:51:37 +0900

    を使用してイベントオブジェクトにアクセスし、追加の機能や処理を行う例

--- src/components/Event2.vue
+++ src/components/Event2.vue
@@ -2,10 +2,13 @@
 const onclick = (message, e) => {
   console.log(message)
   console.log(e)
+  e.target.textContent = "クリックされました！";
 }
 </script>
 
 <template>
+  <!-- $eventはVueテンプレート内でイベントオブジェクトにアクセスするための特別な変数 -->
+  <!-- onclick関数に"こんにちは"と$event（イベントオブジェクト）を渡す -->
   <button v-on:click="onclick('こんにちは', $event)">クリック</button>
 </template>
 
